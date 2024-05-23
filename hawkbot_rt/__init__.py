412e1190d1cab167ddad56f867c7a250889fcbc5
Hawkeye <check@discord.com>
2024-05-23 11:25:42 +0200

    Added further docker support

--- hawkbot_rt/__init__.py
+++ hawkbot_rt/__init__.py
@@ -1,4 +1,4 @@
-# Pyarmor 8.5.2 (pro), 005619, 2024-05-23T10:26:38.748302
+# Pyarmor 8.5.2 (pro), 005619, 2024-05-23T11:22:57.292995
 from sys import version_info as py_version
 def __pyarmor__():
     import platform

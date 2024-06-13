763f4fa3eb2073b6b5194949d0704226cc71fe6b
Hawkeye <check@discord.com>
2024-06-13 11:48:21 +0200

    Fixed issue for bybit where it wasn't properly retrieving candle history further than a few months

--- hawkbot_rt/__init__.py
+++ hawkbot_rt/__init__.py
@@ -1,4 +1,4 @@
-# Pyarmor 8.5.9 (pro), 005619, 2024-06-13T10:45:14.425843
+# Pyarmor 8.5.9 (pro), 005619, 2024-06-13T11:45:36.888590
 from sys import version_info as py_version
 def __pyarmor__():
     import platform

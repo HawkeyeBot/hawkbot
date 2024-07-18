f992cd85053bd2c98cdc8cc9abe5021de9ac4f8a
Hawkeye <check@discord.com>
2024-07-18 14:23:59 +0200

    Print stacktrace when there's an error on startup instead of just the error message

--- hawkbot_rt/__init__.py
+++ hawkbot_rt/__init__.py
@@ -1,4 +1,4 @@
-# Pyarmor 8.5.9 (pro), 005619, 2024-07-18T13:47:03.206420
+# Pyarmor 8.5.9 (pro), 005619, 2024-07-18T14:22:35.634297
 from sys import version_info as py_version
 def __pyarmor__():
     import platform

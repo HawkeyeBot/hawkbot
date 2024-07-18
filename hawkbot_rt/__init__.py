ac57714a4ac76652e5907a6d2574e65cf988e05d
Hawkeye <check@discord.com>
2024-07-18 15:17:41 +0200

    Added fetch_last_24h_changes for bybit

--- hawkbot_rt/__init__.py
+++ hawkbot_rt/__init__.py
@@ -1,4 +1,4 @@
-# Pyarmor 8.5.9 (pro), 005619, 2024-07-18T14:22:35.634297
+# Pyarmor 8.5.9 (pro), 005619, 2024-07-18T15:16:09.865124
 from sys import version_info as py_version
 def __pyarmor__():
     import platform
